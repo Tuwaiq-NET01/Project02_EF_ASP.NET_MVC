b8c4defe778110e39b8a67828d7f610b6c68c93b
Albandry AlQaseemi <Albandr.alqaseemi@gmail.com>
2021-06-05 17:36:55 +0300

    commit files

---
+++ Paradise/Paradise/Controllers/CommunityController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Paradise.Data;
+
+
+namespace Paradise.Controllers
+{
+    public class CommunityController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CommunityController(AppDbContext context)
+        {
+            _db = context;
+        }
+        public IActionResult Index()
+        {
+         
+            var Comments = _db.Comments.ToList();
+            ViewData["Comments"] = Comments;
+
+            return View();
+        }
+    }
+}
