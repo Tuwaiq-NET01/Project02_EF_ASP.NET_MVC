8fbb56963e09f2d2bd1ed1c88d5344254e49e3bb
Reema3Alyousef <reema.alyousef@tuwaiq.edu.sa>
2021-06-05 03:01:14 +0300

    uploaded initial files

---
+++ Event_Hall/Event_Hall/Controllers/EventsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Event_Hall.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Event_Hall.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public EventsController(AppDbContext context)
+        {
+            _db = context;
+        }
+        public IActionResult Index()
+        {
+            var DbEvents = _db.Events.ToList();
+            ViewBag.Events = DbEvents;
+            return View();
+        }
+    }
+}
