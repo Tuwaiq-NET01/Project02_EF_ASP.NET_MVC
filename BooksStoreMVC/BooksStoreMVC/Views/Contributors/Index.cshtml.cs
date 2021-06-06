ea8068cfaf99ae3d72e0c6c42105e6779e0ce25b
Nada Al-Otaibi <82446110+NadaAlOtaibi@users.noreply.github.com>
2021-06-06 14:56:28 +0300

    Update and rename BooksStoreMVC/BooksStoreMVC/Views/Contributor/Index.cshtml.cs to BooksStoreMVC/BooksStoreMVC/Views/Contributors/Index.cshtml.cs

--- BooksStoreMVC/BooksStoreMVC/Views/Contributors/Index.cshtml.cs
+++ BooksStoreMVC/BooksStoreMVC/Views/Contributors/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BooksStoreMVC.Views.Contributor
+namespace BooksStoreMVC.Views.Contributors
 {
     public class IndexModel : PageModel
     {
