8ee1da7b915362ed8c35f4a474f2bafb54906915
Nada Al-Otaibi <82446110+NadaAlOtaibi@users.noreply.github.com>
2021-06-06 14:52:39 +0300

    Update and rename BooksStoreMVC/BooksStoreMVC/Views/Book/Index.cshtml.cs to BooksStoreMVC/BooksStoreMVC/Views/Books/Index.cshtml.cs

--- BooksStoreMVC/BooksStoreMVC/Views/Books/Index.cshtml.cs
+++ BooksStoreMVC/BooksStoreMVC/Views/Books/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BooksStoreMVC.Views.Publisher
+namespace BooksStoreMVC.Views.Publishers
 {
     public class IndexModel : PageModel
     {
